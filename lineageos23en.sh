bd255453dd7402640ddf8c33c942a48442b16ad4
Dudu-gsh <fse.sansai@gmail.com>
2026-08-19 12:21:49 -0300

    I fixed or added somthing. Time of commit:19-08-26 12:21

--- lineageos23en.sh
+++ lineageos23en.sh
@@ -129,7 +129,7 @@ read -rp "Want to compile a Signed Build (Y/N)" SIGNED
    if [[ $SIGNED =~ ^[Yy]$ ]]; then 
 cd "$HERE"
 chmod +x sign_please
-./sign_please.sh
+./sign_please.sh "$EMAIL"
 
    else
 brunch "$CODENOME"
